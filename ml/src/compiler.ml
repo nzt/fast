c62e4c07f6e2e9efac3de64d5949cbc0cdb85f0d
TANIGUCHI Masaya <mew@cat.ovh>
2019-10-14 02:25:06 +0900

    Update compiler

--- ml/src/compiler.ml
+++ ml/src/compiler.ml
@@ -6,15 +6,14 @@ type expression =
 let rec generate s i =
     match s with
     | Fexp (operand1, operand2) -> 
-        let (j, t) = generate operand2 (i + 1) in
-            (j + 1, t @ [Inpterpreter.Fop(Char.code operand1.[0], j)])
+        let (j, t) = generate operand2 i in
+            (j + 1, t @ [Inpterpreter.Fop(Char.code operand1.[0], j - 1)])
     | Aexp (operand1, operand2) ->
-        let (j, t) = generate operand1 (i + 1) in
-        let (k, u) = generate operand2 (j + 1) in
-            (k + 1, t @ u @ [Inpterpreter.Aop(j, k)])
+        let (j, t) = generate operand1 i in
+        let (k, u) = generate operand2 j in
+            (k + 1, t @ u @ [Inpterpreter.Aop(j - 1, k - 1)])
     | Sexp (operand1) ->
-        let t = Char.code operand1.[0] in
-            (i + 1, [Inpterpreter.Sop(t, 0)])
+        (i + 1, [Inpterpreter.Sop(Char.code operand1.[0], 0)])
 
 let compile s =
     let (i, t) = generate s 0 in
